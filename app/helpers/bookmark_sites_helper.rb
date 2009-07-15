2a5ee05e8238681cfe2214a6fb0af423b7a829b3
Windix Feng <windix@gmail.com>
2009-07-15 11:01:53 +1000

    add environment-based settings

--- app/helpers/bookmark_sites_helper.rb
+++ app/helpers/bookmark_sites_helper.rb
@@ -1,7 +1,7 @@
 module BookmarkSitesHelper
   def bookmarklet
     # TODO
-    "javascript:(function(){f='http://localhost:3000/bookmark_sites/new?url='+encodeURIComponent(window.location.href)+'&title='+encodeURIComponent(document.title);a=function(){location.href=f};if(/Firefox/.test(navigator.userAgent)){setTimeout(a,0)}else{a()}})()"
+    "javascript:(function(){f='#{WEBSITE_URL}/bookmarks/new?url='+encodeURIComponent(window.location.href)+'&title='+encodeURIComponent(document.title);a=function(){location.href=f};if(/Firefox/.test(navigator.userAgent)){setTimeout(a,0)}else{a()}})()"
   end
   
   def versions_tag(site)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,3 +26,5 @@ config.action_view.cache_template_loading            = true
 
 # Enable threaded mode
 # config.threadsafe!
+
+WEBSITE_URL = "http://mirror.douzi.org"
