3b81614e4df30c299351751415c17fb3beb4f220
Windix Feng <windix@gmail.com>
2009-07-20 10:49:56 +1000

    User can edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_filter :require_invitation_token
+  before_filter :require_invitation_token, :only => [ :new, :create ]
 
   # render new.rhtml
   def new
@@ -18,10 +18,34 @@ class UsersController < ApplicationController
       # reset session
       self.current_user = @user # !! now logged in
       redirect_back_or_default('/')
-      flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
+      flash[:notice] = "Thanks for signing up!" #  We're sending you an email with your activation code."
     else
       flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
       render :action => 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    
+    if (params[:user][:password].blank? && params[:user][:password_confirmation].blank?)
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "Profile was successfully updated."
+      redirect_to(@user)
+    else
+      render :action => 'edit'
     end
   end
 
